afb3c0d5ce0be41d547976076db6df20def58edc
Dmitriy982 <nomad.aleks777@gmail.com>
2023-12-27 02:02:50 +0300

    FirstTry

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardInput = document.querySelector('.places__list');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+initialCards.forEach(function (element) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = element.name;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+    cardInput.append(cardElement);
 
-// @todo: Вывести карточки на страницу
+    deleteButton.addEventListener('click', function (evt) {
+        const eventTarget = evt.target.closest('.places__item');
+        remove__card(eventTarget);
+      }); 
+  })
+
+  
+function remove__card (eventTarget) {
+    eventTarget.remove();
+}
+
+
