ff4f892aa37485316f56212d41ff9682c7c0a027
Dmitriy982 <nomad.aleks777@gmail.com>
2023-12-27 20:28:04 +0300

    SecondTry

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardInput = document.querySelector('.places__list');
 
-
-initialCards.forEach(function (element) {
-    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    
-    cardElement.querySelector('.card__image').src = element.link;
-    cardElement.querySelector('.card__image').alt = element.name;
-    cardElement.querySelector('.card__title').textContent = element.name;
-    
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-
-    cardInput.append(cardElement);
-
-    deleteButton.addEventListener('click', function (evt) {
-        const eventTarget = evt.target.closest('.places__item');
-        remove__card(eventTarget);
-      }); 
-  })
-
-  
-function remove__card (eventTarget) {
+function removeCard (eventTarget) {
     eventTarget.remove();
 }
 
 
+function createCard (element) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = element.link; 
+    cardImage.alt = element.name; 
+    cardElement.querySelector('.card__title').textContent = element.name;
+    
+    deleteButton.addEventListener('click', function (evt) {
+        const eventTarget = evt.target.closest('.places__item');
+        removeCard(eventTarget);
+      }); 
+    
+    return cardElement;
+}
+
+
+initialCards.forEach(function (element){
+    cardInput.append(createCard(element));
+});
